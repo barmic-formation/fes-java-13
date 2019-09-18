1e5b7092357a3c668ff67e4ae6b0a024dbdfc0ac
Michel Barret <michel.barret@gmail.com>
2019-09-18 23:52:59 +0200

    some example

--- src/test/java/com/viseo/fes/java/LocalThreadSotrage.java
+++ src/test/java/com/viseo/fes/java/LocalThreadSotrage.java
@@ -1,7 +1,54 @@
 package com.viseo.fes.java;
 
+import com.google.common.flogger.FluentLogger;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 /**
  * Present local thread storage
  */
-public class LocalThreadSotrage {
+class LocalThreadSotrage {
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+    private static class Foo {
+        private static ThreadLocal<Integer> threadLocalValue = new ThreadLocal<>();
+
+        int value() {
+            return threadLocalValue.get();
+        }
+
+        void value(int v) {
+            threadLocalValue.set(v);
+        }
+    }
+
+    @Test
+    void foo() throws ExecutionException, InterruptedException {
+        new Thread(() -> {
+            Foo f1 = new Foo();
+            f1.value(12);
+            while (true) {
+                try {
+                    Thread.sleep(1_000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                logger.atInfo().log("plouf %d", f1.value());
+            }
+        }).start();
+        new Thread(() -> {
+            Foo f1 = new Foo();
+            f1.value(42);
+            while (true) {
+                try {
+                    Thread.sleep(1_000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                logger.atInfo().log("plaf %d", f1.value());
+            }
+        }).start();
+        new CompletableFuture<>().get();
+    }
 }
--- src/test/java/com/viseo/fes/java/Proxy.java
+++ src/test/java/com/viseo/fes/java/Proxy.java
@@ -1,4 +1,24 @@
 package com.viseo.fes.java;
 
-public class Proxy {
+import com.google.common.flogger.FluentLogger;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Proxy {
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+    @FunctionalInterface
+    interface Toto {
+        Integer get();
+    }
+    @Test
+    void foo() {
+        List<Class<Toto>> classes = List.of(Toto.class);
+        Toto t = () -> 45;
+        Toto coucou = (Toto)java.lang.reflect.Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), classes.toArray(new Class<?>[0]), (proxy, method, args) -> {
+            logger.atInfo().log("coucou");
+            return t.get();
+        });
+        logger.atInfo().log("coucou %d", coucou.get());
+    }
 }
